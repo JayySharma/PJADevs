7d170e7bb8b409c6c6e44e493573484fcf825eab
jayy-sharma <53940605+jayy-sharma@users.noreply.github.com>
2020-10-15 00:43:45 +0530

    initially final commit

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$businessName = $_POST["businessName"];
+$contactno=$_POST["contactno"];
+$email = $_POST["email"];
+$businessType=$_POST["businessType"];
+$anyMessage=$_POST["anyMessage"];
+
+$conn = new mysqli('localhost', 'root', '','blehforms');
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}else{
+$stmt=$conn->prepare("insert into businessreg(fname,lname,businessName,contactno,email,businessType,anyMessage)values(?,?,?,?,?,?,?)");
+$stmt->bind_param("sssssss",$fname,$lname,$businessName,$contactno,$email,$businessType,$anyMessage);
+$stmt->execute();
+echo "data entered..";
+$stmt->close();
+$conn->close();
+}
+?>
